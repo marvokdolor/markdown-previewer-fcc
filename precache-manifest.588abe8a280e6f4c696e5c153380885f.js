6b3206d406f94a410b3b966e3a2f97f1e1d5299f
marvokdolor <marvourneen@mkdolor.com>
2018-12-28 13:32:19 -0500

    Updates

---
+++ precache-manifest.588abe8a280e6f4c696e5c153380885f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d70b42fa126d0e725c52",
+    "url": "/markdown-previewer-fcc/static/js/runtime~main.d70b42fa.js"
+  },
+  {
+    "revision": "a13f250fccfa66373c89",
+    "url": "/markdown-previewer-fcc/static/js/main.a13f250f.chunk.js"
+  },
+  {
+    "revision": "1227f48283e830ce7435",
+    "url": "/markdown-previewer-fcc/static/js/1.1227f482.chunk.js"
+  },
+  {
+    "revision": "a13f250fccfa66373c89",
+    "url": "/markdown-previewer-fcc/static/css/main.0ac66c5d.chunk.css"
+  },
+  {
+    "revision": "790ff9e611ab11fc92f57f173191f6a1",
+    "url": "/markdown-previewer-fcc/index.html"
+  }
+];
