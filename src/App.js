4fad7b18d9613bb5bd02ba9a57eb5f8a161c4332
marvokdolor <marvourneen@mkdolor.com>
2018-12-28 18:33:12 -0500

    updated css and deleted docs folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './style.css';
+// import hljs from 'highlight.js';
 
 // const Remarkable = require('remarkable');
 const Marked = require('marked');
 
 Marked.setOptions({
     renderer: new Marked.Renderer(),
-    // highlight: function(code) {
-    // return require('highlight.js').highlightAuto(code).value;
-    // },
+    highlight: function(code) {
+        return require('highlight.js').highlightAuto(code).value;
+    },
     pedantic: false,
     gfm: true,
     tables: true,
@@ -35,21 +36,34 @@ class App extends React.Component {
 
     handleMarkUp() {
         // const markup = new Remarkable();
+        // return { __html: markup.render(this.state.value) }
         return { __html: Marked(this.state.value) };
     }
 
     render() {
         return (
             <div>
-                <h2>Go on, enter some more markdown. I've given it a start.</h2>
-                <textarea
-                id="editor"
-                onChange={this.handleChange}
-                defaultValue={this.state.value}
-                />
-                <div id="preview"
-                    dangerouslySetInnerHTML={this.handleMarkUp()}
-                />
+                <div id="container-fluid">
+                    <h1 id="title">Marvo's Markdown Previewer</h1>
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="well">
+                                <textarea
+                                id="editor"
+                                onChange={this.handleChange}
+                                defaultValue={this.state.value}
+                                />
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="well">
+                                <div id="preview"
+                                    dangerouslySetInnerHTML={this.handleMarkUp()}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -57,7 +71,7 @@ class App extends React.Component {
 
 
 const placeholderText =
-`# Welcome to my Markdown Previewer. \n## Thanks for checking it out. \n\n Here's a [link](http://mkdolor.com) to my website. \n
+`# Thanks for checking this out. \n## Go on, enter some more markdown, here or anywhere on the left. \n\n Here's a [link](https://markdownlivepreview.com) to the site that inspired the design of this one. \n
 A little code \`const cool = true;\` and a code block:
 \`\`\`
 constructor(props) {
@@ -70,7 +84,9 @@ A list of exciting things:
   - bullets
     - etc.
 
-> We can do quotes, like this one.
+Test text with no markdown.
+>We can do quotes, like this one. _Unfortunately, the greying out effect doesn't appear to be working right now. The indentation is though. **Hooray**!_
+
 And **bold** words like that one.
 And last, but not least we can embed images:
 ![freeCodeCamp Logo](https://github.com/FreeCodeCamp/assets/blob/master/assets/logos/600x72%20Free%20Code%20Camp%20logo%20for%20Medium%20publication.png?raw=true)
