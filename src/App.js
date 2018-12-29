c16394a519733bad80ef728607991531a0517ece
marvokdolor <marvourneen@mkdolor.com>
2018-12-29 00:05:08 -0500

    updated styling

--- src/App.js
+++ src/App.js
@@ -44,22 +44,24 @@ class App extends React.Component {
         return (
             <div>
                 <div id="container-fluid">
-                    <h1 id="title">Marvo's Markdown Previewer</h1>
-                    <div class="row">
-                        <div class="col-xs-6">
-                            <div class="well">
-                                <textarea
-                                id="editor"
-                                onChange={this.handleChange}
-                                defaultValue={this.state.value}
-                                />
+                    <h1 id="title">Markdown Previewer</h1>
+                    <div id="frame">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="well">
+                                    <textarea
+                                    id="editor"
+                                    onChange={this.handleChange}
+                                    defaultValue={this.state.value}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="well">
-                                <div id="preview"
-                                    dangerouslySetInnerHTML={this.handleMarkUp()}
-                                />
+                            <div class="col-sm-6">
+                                <div class="well">
+                                    <div id="preview"
+                                        dangerouslySetInnerHTML={this.handleMarkUp()}
+                                    />
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -71,7 +73,8 @@ class App extends React.Component {
 
 
 const placeholderText =
-`# Thanks for checking this out. \n## Go on, enter some more markdown, here or anywhere on the left. \n\n Here's a [link](https://markdownlivepreview.com) to the site that inspired the design of this one. \n
+`# Thanks for checking this out. \n---\n## Go on, enter some more markdown anywhere in the box on the left. \n\n ---\nHere's a [link](https://markdownlivepreview.com) to the site that inspired the design of this one. \n
+
 A little code \`const cool = true;\` and a code block:
 \`\`\`
 constructor(props) {
@@ -84,12 +87,19 @@ A list of exciting things:
   - bullets
     - etc.
 
+1. bullet
+1. bullets
+1. bullets
+1. .
+
 Test text with no markdown.
->We can do quotes, like this one. _Unfortunately, the greying out effect doesn't appear to be working right now. The indentation is though. **Hooray**!_
+> We can do quotes, like this one. _Unfortunately, the greying out effect and indentation don't appear to be working right now._
 
 And **bold** words like that one.
+
 And last, but not least we can embed images:
-![freeCodeCamp Logo](https://github.com/FreeCodeCamp/assets/blob/master/assets/logos/600x72%20Free%20Code%20Camp%20logo%20for%20Medium%20publication.png?raw=true)
+![a couple owls](http://clipart-library.com/img/2104391.png)
 `
+
 
 export default App
